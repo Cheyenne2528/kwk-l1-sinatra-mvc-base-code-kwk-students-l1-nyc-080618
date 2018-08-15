0e636c53426666c61b094e8d9143b2cb5c05e48f
Cheyenne2528 <cheyennefun12@gmail.com>
2018-08-15 18:20:06 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -6,5 +6,13 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  get "/results" do
+    erb :results
+  end
+  
+  post "/output" do
+    "Your score is 5 out of 10"
+  end 
 
 end
